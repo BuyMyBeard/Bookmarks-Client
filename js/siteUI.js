ab520ba8ed45e2b2f59e3beccd92e819ea2f7262
BuyMyBeard <95039323+BuyMyBeard@users.noreply.github.com>
2023-09-11 14:14:17 -0400

    first commit

---
+++ js/siteUI.js
@@ -0,0 +1,290 @@
+//<span class="cmdIcon fa-solid fa-ellipsis-vertical"></span>
+let contentScrollPosition = 0;
+Init_UI();
+
+function Init_UI() {
+    renderBookmarks();
+    $('#createBookmark').on("click", async function () {
+        saveContentScrollPosition();
+        renderCreateBookmarkForm();
+    });
+    $('#abort').on("click", async function () {
+        renderBookmarks();
+    });
+    $('#aboutCmd').on("click", function () {
+        renderAbout();
+    });
+    $('#showAllCategoriesCmd').on("click", () => renderBookmarks());
+    $("#appLogo").on("click", () => renderBookmarks());
+}
+
+function renderAbout() {
+    saveContentScrollPosition();
+    eraseContent();
+    $("#createBookmark").hide();
+    $("#abort").show();
+    $("#actionTitle").text("À propos...");
+    $("#content").append(
+        $(`
+            <div class="aboutContainer">
+                <h2>Gestionnaire de bookmarks</h2>
+                <hr>
+                <p>
+                    Petite application de gestion de bookmarks à titre de démonstration
+                    d'interface utilisateur monopage réactive.
+                </p>
+                <p>
+                    Auteur: Alexandre Lacombe
+                </p>
+                <p>
+                    Collège Lionel-Groulx, automne 2023
+                </p>
+            </div>
+        `))
+}
+async function renderBookmarks(category) {
+    showWaitingGif();
+    $("#actionTitle").text("Liste des favoris");
+    $("#createBookmark").show();
+    $("#abort").hide();
+    let bookmarks = await API_GetBookmarks();
+    eraseContent();
+    clearCategories();
+    if (bookmarks !== null) {
+        const categories = getBookmarksCategories(bookmarks);
+        const insertPoint = $("#categoriesInsertPoint");
+        categories.forEach((category) => insertPoint.before(renderBookmarkCategoryButton(category)));
+        bookmarks.forEach(bookmark => {
+            if (category === undefined || category === bookmark.Category) {
+                $("#content").append(renderBookmark(bookmark));
+            }
+        });
+        restoreContentScrollPosition();
+        // Attached click events on command icons
+        $(".editCmd").on("click", function () {
+            saveContentScrollPosition();
+            renderEditBookmarkForm(parseInt($(this).attr("editBookmarkId")));
+        });
+        $(".deleteCmd").on("click", function () {
+            saveContentScrollPosition();
+            renderDeleteBookmarkForm(parseInt($(this).attr("deleteBookmarkId")));
+        });
+        $(".bookmarkCategory").each((i, b) => {
+            const button = $(b);
+            button.on("click", (e) => {
+                renderBookmarks(button.text());
+            });
+        });
+    } else {
+        renderError("Service introuvable");
+    }
+}
+function showWaitingGif() {
+    $("#content").empty();
+    $("#content").append($("<div class='waitingGifcontainer'><img class='waitingGif' src='Loading_icon.gif' /></div>'"));
+}
+function eraseContent() {
+    $("#content").empty();
+}
+function saveContentScrollPosition() {
+    contentScrollPosition = $("#content")[0].scrollTop;
+}
+function restoreContentScrollPosition() {
+    $("#content")[0].scrollTop = contentScrollPosition;
+}
+function renderError(message) {
+    eraseContent();
+    $("#content").append(
+        $(`
+            <div class="errorContainer">
+                ${message}
+            </div>
+        `)
+    );
+}
+function renderCreateBookmarkForm() {
+    renderBookmarkForm();
+}
+async function renderEditBookmarkForm(id) {
+    showWaitingGif();
+    let bookmark = await API_GetBookmark(id);
+    if (bookmark !== null)
+        renderBookmarkForm(bookmark);
+    else
+        renderError("Favori introuvable!");
+}
+async function renderDeleteBookmarkForm(id) {
+    showWaitingGif();
+    $("#createBookmark").hide();
+    $("#abort").show();
+    $("#actionTitle").text("Retrait");
+    let bookmark = await API_GetBookmark(id);
+    eraseContent();
+    if (bookmark !== null) {
+        $("#content").append(`
+        <div class="bookmarkdeleteForm">
+            <h4>Effacer le favori suivant?</h4>
+            <br>
+            <div class="bookmarkRow" bookmark_id=${bookmark.Id}">
+                <div class="bookmarkContainer">
+                <div class="bookmarkLayout">
+                <div>
+                    <a class="bookmarkUrl" href="${bookmark.Url}" target="_blank"><image class="bookmarkFavicon" src="${getFavicon(bookmark.Url)}"></a>
+                    <span class="bookmarkTitle">${bookmark.Title}</span>
+                </div>
+                <div class="bookmarkCategory btn btn-link">${bookmark.Category}</div>
+            </div>
+                </div>  
+            </div>   
+            <br>
+            <input type="button" value="Effacer" id="deleteBookmark" class="btn btn-primary">
+            <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
+        </div>    
+        `);
+        $('#deleteBookmark').on("click", async function () {
+            showWaitingGif();
+            let result = await API_DeleteBookmark(bookmark.Id);
+            if (result)
+                renderBookmarks();
+            else
+                renderError("Une erreur est survenue!");
+        });
+        $('#cancel').on("click", function () {
+            renderBookmarks();
+        });
+        $('.bookmarkCategory').each((i, b) => {
+            const button = $(b);
+            button.on('click', (e) => renderBookmarks(button.text()));
+        });
+    } else {
+        renderError("Favori introuvable!");
+    }
+}
+function newBookmark() {
+    const bookmark = {};
+    bookmark.Id = 0;
+    bookmark.Title = "";
+    bookmark.Url = "";
+    bookmark.Category = "";
+    return bookmark;
+}
+function renderBookmarkForm(bookmark = null) {
+    $("#createBookmark").hide();
+    $("#abort").show();
+    eraseContent();
+    let create = bookmark == null;
+    if (create) bookmark = newBookmark();
+    $("#actionTitle").text(create ? "Création" : "Modification");
+    $("#content").append(`
+    <form class="form" id="bookmarkForm">
+            <img id="iconPreview" src="${bookmark.Url === "" ? "favicon.ico" : getFavicon(bookmark.Url)}"><br>
+            <input type="hidden" name="Id" value="${bookmark.Id}"/>
+
+            <label for="Title" class="form-label">Nom </label>
+            <input 
+                class="form-control AlphaNumerical"
+                name="Title" 
+                id="Title" 
+                placeholder="Titre"
+                required
+                RequireMessage="Veuillez entrer un titre"
+                InvalidMessage="Le nom comporte un caractère illégal" 
+                value="${bookmark.Title}"
+            />
+            <label for="Url" class="form-label">Url</label>
+            <input
+                class="form-control Url"
+                name="Url"
+                id="Url"
+                placeholder="Url"
+                required
+                RequireMessage="Veuillez entrer un url" 
+                InvalidMessage="Veuillez entrer un url valide"
+                value="${bookmark.Url}" 
+            />
+            <label for="Category" class="form-label">Catégorie</label>
+            <input 
+                class="form-control AlphaNumerical"
+                name="Category"
+                id="Category"
+                placeholder="Catégorie"
+                required
+                RequireMessage="Veuillez entrer une catégorie" 
+                InvalidMessage="Veuillez entrer une catégorie valide"
+                value="${bookmark.Category}"
+            />
+            <br>
+            <input type="submit" value="Enregistrer" id="saveBookmark" class="btn btn-primary">
+            <input type="button" value="Annuler" id="cancel" class="btn btn-secondary">
+        </form>
+    `);
+    initFormValidation();
+    $('#bookmarkForm').on("submit", async function (event) {
+        event.preventDefault();
+        let bookmark = getFormData($("#bookmarkForm"));
+        bookmark.Id = parseInt(bookmark.Id);
+        showWaitingGif();
+        let result = await API_SaveBookmark(bookmark, create);
+        if (result)
+            renderBookmarks();
+        else
+            renderError("Une erreur est survenue!");
+    });
+    $('#cancel').on("click", function () {
+        renderBookmarks();
+    });
+    $('#Url').on("change" ,(e) => {
+        let url = e.currentTarget.value;
+        if (url === "") $('#iconPreview').attr("src", "favicon.ico");
+        else $('#iconPreview').attr("src", getFavicon(url));
+    });
+}
+
+function getFormData($form) {
+    const removeTag = new RegExp("(<[a-zA-Z0-9]+>)|(</[a-zA-Z0-9]+>)", "g");
+    var jsonObject = {};
+    $.each($form.serializeArray(), (index, control) => {
+        jsonObject[control.name] = control.value.replace(removeTag, "");
+    });
+    return jsonObject;
+}
+
+function renderBookmark(bookmark) {
+    return $(`
+     <div class="bookmarkRow" bookmark_id=${bookmark.Id}">
+        <div class="bookmarkContainer noselect">
+            <div class="bookmarkLayout">
+                <div>
+                    <a class="bookmarkUrl" href="${bookmark.Url}" target="_blank"><image class="bookmarkFavicon" src="${getFavicon(bookmark.Url)}"></a>
+                    <span class="bookmarkTitle">${bookmark.Title}</span>
+                </div>
+                <div class="bookmarkCategory btn btn-link">${bookmark.Category}</div>
+            </div>
+            <div class="bookmarkCommandPanel">
+                <span class="editCmd cmdIcon fa fa-pencil" editBookmarkId="${bookmark.Id}" title="Modifier ${bookmark.Title}"></span>
+                <span class="deleteCmd cmdIcon fa fa-trash" deleteBookmarkId="${bookmark.Id}" title="Effacer ${bookmark.Title}"></span>
+            </div>
+        </div>
+    </div>           
+    `);
+
+}
+
+function getFavicon(url, size = 256) {
+    return `https://www.google.com/s2/favicons?domain=${url}&sz=${size}`;
+}
+
+function getBookmarksCategories(bookmarks) {
+    const categories = new Set();
+    bookmarks.forEach((bookmark) => categories.add(bookmark.Category));
+
+    return categories;
+}
+
+function renderBookmarkCategoryButton(category) {
+    return `<div class="dropdown-item categoryButton bookmarkCategory">${category}</div>`;
+}
+
+function clearCategories() {
+    $(".categoryButton").each((i, b) => b.remove());
+}
